c477599a266f30bf966e94c287c04fd046d0041c
martialarcade <152780571+martialarcade@users.noreply.github.com>
2024-03-06 22:48:51 +0000

    Update playerStates.js

--- js/playerStates.js
+++ js/playerStates.js
@@ -413,7 +413,7 @@ export class Hit extends State {
       if (this.player.frameX === 0) this.game.addBlood(this.player);
       if (this.game.mode === 'kumite' && this.player.health>0) this.player.health-=((this.player.enemies[0].attack-this.player.defense)*2);
     }
-    //if (this.player.frameX === 0) getSfx(this.player.enemies[0].currentState.state, this.game);
+    if (this.player.frameX === 0) getSfx(this.player.enemies[0].currentState.state, this.game);
   }
   handleInput(input) {
     if (this.player.health <= 0) {
@@ -441,7 +441,7 @@ export class Fall extends State {
       if (this.player.frameX === 0) this.game.addBlood(this.player);
       if (this.game.mode === 'kumite' && this.player.health>0) this.player.health-=((this.player.enemies[0].attack-this.player.defense)*2);
     }
-    //this.game.audioHit3.play();
+    this.game.audioHit3.play();
   }
   handleInput(input) {
     if (this.player.frameX >= this.player.maxFrame) {
