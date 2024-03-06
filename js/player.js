c0445bc60ef99b0c00ca2ba819a33127ccb79356
martialarcade <152780571+martialarcade@users.noreply.github.com>
2024-03-06 13:45:04 +0000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -66,6 +66,8 @@ export default class Player {
     this.endLoop = 0;
   }
   update(input, deltaTime) {
+    //audio
+    if (this.currentState.state === 'HIT' && this.frameX === 0) this.game.audioHit1.play();
     //sprite animation
     if (this.frameTimer > this.frameInterval) {
       //walk direction
